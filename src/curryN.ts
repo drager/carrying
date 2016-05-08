85ba80b4c3b73b395876339fc45975b5d70b07f5
Jesper Håkansson <jesper@jesperh.se>
2016-05-08 18:13:11 +0200

    refactor: Make it shorter

--- src/curryN.ts
+++ src/curryN.ts
@@ -1,7 +1,6 @@
-export const curryN = (n: number) => (fn: Function, ...args) =>  {
-  return function curried(...args) {
+export const curryN = (n: number) => (fn: Function, ...args) =>
+  function curried(...args) {
     return args.length >= fn.length
       ? fn.call(this, ...args)
       : (...rest) => curried.call(this, ...args, ...rest)
   }
-}
