c467f563750cd7524c2c679eb312aee626472d9c
Jesper Håkansson <jesper@jesperh.se>
2016-05-08 23:21:51 +0200

    Expose more functions

--- src/index.ts
+++ src/index.ts
@@ -6,5 +6,13 @@ export {curryN} from './curryN'
 
 export {flip} from './flip'
 
+export {head} from './head'
+
+export {nullAware} from './nullAware'
+
+export {range} from './range'
+
+export {sum} from './sum'
+
 // Zipping
 export {zip} from './zip'
