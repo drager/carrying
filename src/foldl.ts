fdbc0f34a462e26021db56d9576d942b75306333
Jesper Håkansson <drager@programmers.se>
2016-05-09 12:52:43 +0200

    feat: Add foldl function

---
+++ src/foldl.ts
@@ -0,0 +1,28 @@
+import {head} from './head'
+/**
+ * Applies a function against an accumulator and each value of the list from left-to-right
+ * to reduce it to a single value. (also known variously as reduce).
+ *
+ * @function
+ * @since v0.1.0
+ * @param {Function} fn - The accumlating function, the function to execute on each value in the array
+ * @param {Array} list - The list we should accumlate
+ * @param {any} accumlator -
+ * @param {number} index -
+ * @return {any}
+ * @example
+ *   const add = (prev, current) => prev + current
+ *   foldl(add)([1, 2, 3]) // => 6
+ */
+export const foldl = (fn: Function) => (list, accumulator = head(list), index = 0) => {
+  let result
+  if (index === list.length - 1) {
+    result = accumulator
+  } else {
+    accumulator = fn(accumulator, list[index + 1])
+    result = foldl(fn)(list, accumulator, index + 1)
+  }
+
+  return result
+}
+
---
+++ test/foldl.spec.ts
@@ -0,0 +1,9 @@
+import {expect} from 'chai'
+import {foldl} from '../src/foldl'
+
+describe('foldl', () => {
+  it('should apply the provided function', () => {
+    const add = (prev, current) => prev + current
+    expect(foldl(add)([1, 2, 3])).to.equal(6)
+  })
+})
