422582a9cca1b2fe61393401f82dcc29d88a3c18
Jesper Håkansson <drager@programmers.se>
2016-05-23 09:05:15 +0200

    expose more functions

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,13 @@ export {curryN} from './curryN'
 
 export {flip} from './flip'
 
+export {foldl} from './foldl'
+
+// Array functions
 export {head} from './head'
+export {last} from './last'
+export {tail} from './tail'
+export {uncons} from './uncons'
 
 export {nullAware} from './nullAware'
 
