62b14333456fb770d99a0104d8e8c4295af40cbb
Jesper Håkansson <drager@programmers.se>
2016-05-24 16:46:02 +0200

    add comment to helper function

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,9 @@
 import {curry} from './curry'
 
+/**
+ * Helper function that helps to iterate over an list
+ * with a given start, stop and step.
+ */
 export const iterate = curry((start, stop, step, list) => {
   const newList = [...list]
   for (let i = start; i < stop; i += step) {
