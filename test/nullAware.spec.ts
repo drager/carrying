29e2cc3128638af2ba273098e94e3f9ac941d3ca
Jesper Håkansson <drager@programmers.se>
2016-05-25 16:17:05 +0200

    test(nullAware): Add another test case

--- test/nullAware.spec.ts
+++ test/nullAware.spec.ts
@@ -16,8 +16,12 @@ describe('nullAware', () => {
   })
 
   it('should return handle more types than strings', () => {
-    expect(nullAware(null, {name: 'Aware'}), {name: 'Aware'})
-    expect(nullAware(null, 50), 50)
-    expect(nullAware(null, [1, 5, 10]), [1, 5, 10])
+    expect(nullAware(null, {name: 'Aware'})).to.deep.equal({name: 'Aware'})
+    expect(nullAware(null, 50)).to.equal(50)
+    expect(nullAware(null, [1, 5, 10])).to.deep.equal([1, 5, 10])
+  })
+
+  it('should return the supposed to be null value if it is not null', () => {
+    expect(nullAware(1, 'this will not be used')).to.equal(1)
   })
 })
