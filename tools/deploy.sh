e12a3c3b2a5c0e129a98c8f59d547b1382e222e6
Jesper Håkansson <drager@programmers.se>
2016-05-31 13:32:55 +0200

    chore(travis): Generate documentation after success

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  exit 0
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+  exit 0
+fi
+
 set -e
 rm -rf docs
 npm run generate-docs
 cd docs
 git init
-git remote add origin git@github.com:drager/carrying.git
 git checkout -b gh-pages
+git config --global user.email "jesper@jesperh.se"
+git config --global user.name "travis-ci"
+git remote add deploy "https://$GITHUB_AUTH@github.com/drager/carrying.git"
+
 git add -A
-git commit -am 'Deploy'
-git push origin gh-pages --force
+
+git commit -am "Deploy of build #$TRAVIS_BUILD_NUMBER of commit $TRAVIS_COMMIT"
+git push deploy gh-pages --force > /dev/null 2>&1
